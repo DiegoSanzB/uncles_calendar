266ec777b7f449f58c0d76b7eb6c847e2e82cdb0
bbrisch <benjaminbritoschiele@gmail.com>
2023-01-25 23:20:40 -0300

    Planificación de layouts para gui

---
+++ gui.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')   # Add a touch of color
+
+# Generamos un layout
+
+# layout de inicio
+
+
+# layout de dedcación
+
+
+# layout de generación de calendario
+ 
+
+
+
